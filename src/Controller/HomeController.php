4a4e61546d2568d023b4a0b7bf13eb1ae8dd1762
Kevin <herrbach.kevin@gmail.com>
2019-01-02 17:17:51 +0100

    Handles submission of a new counter

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -13,16 +13,29 @@ use App\Entity\Counter;
 use App\Form\CounterType;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 
 class HomeController extends AbstractController
 {
     /**
      * @Route("/", name="home")
+     * @param Request $request
+     * @return \Symfony\Component\HttpFoundation\RedirectResponse|\Symfony\Component\HttpFoundation\Response
      */
-    public function index()
+    public function index(Request $request)
     {
         $counter = new Counter();
         $form = $this->createForm(CounterType::class, $counter);
+
+        $form->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid()) {
+            $counter = $form->getData();
+            $manager = $this->getDoctrine()->getManager();
+            $manager->persist($counter);
+            $manager->flush();
+
+            return $this->redirectToRoute('home');
+        }
 
         return $this->render('home.html.twig', [
            'form' => $form->createView(),
